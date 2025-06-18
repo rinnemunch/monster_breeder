eb5c3c4f94fb05debf94391d4151157d0ccd35a2
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-18 12:50:02 -0400

    Added breedMonsters function to combine traits from two parents

--- go_backend/main.go
+++ go_backend/main.go
@@ -8,6 +8,14 @@ import (
 	"time"
 )
 
+// no built in for max
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 type Monster struct {
 	Name       string
 	Color      string
@@ -31,6 +39,61 @@ func generateRandomMonster() Monster {
 		Speed:      rand.Intn(100) + 1,
 		Rarity:     rarities[rand.Intn(len(rarities))],
 		Generation: 1,
+	}
+}
+
+func breedMonsters(parent1, parent2 Monster) Monster {
+	// Randomly inherit Name and Color
+	var name string
+	if rand.Intn(2) == 0 {
+		name = parent1.Name
+	} else {
+		name = parent2.Name
+	}
+
+	var color string
+	if rand.Intn(2) == 0 {
+		color = parent1.Color
+	} else {
+		color = parent2.Color
+	}
+
+	// Blend strength and speed
+	strength := (parent1.Strength + parent2.Strength) / 2
+	speed := (parent1.Speed + parent2.Speed) / 2
+
+	// Rarity
+	var rarity string
+	if rarityRank(parent1.Rarity) > rarityRank(parent2.Rarity) {
+		rarity = parent1.Rarity
+	} else {
+		rarity = parent2.Rarity
+	}
+
+	return Monster{
+		Name:       name,
+		Color:      color,
+		Strength:   strength,
+		Speed:      speed,
+		Rarity:     rarity,
+		Generation: max(parent1.Generation, parent2.Generation) + 1,
+	}
+}
+
+func rarityRank(r string) int {
+	switch r {
+	case "Common":
+		return 1
+	case "Uncommon":
+		return 2
+	case "Rare":
+		return 3
+	case "Epic":
+		return 4
+	case "Legendary":
+		return 5
+	default:
+		return 0
 	}
 }
 
