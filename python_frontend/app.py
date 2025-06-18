2698f34e644a8693d8e0b3fd34217fa8d6f6ab5a
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-18 12:39:10 -0400

    Python frontend now requests random monster from Go API

--- python_frontend/app.py
+++ python_frontend/app.py
@@ -1,1 +1,10 @@
-print("Python frontend ready.")
+import requests
+
+response = requests.get("http://localhost:8080/new")
+
+if response.status_code == 200:
+    monster = response.json()
+    print("Got a monster from Go!")
+    print(monster)
+else:
+    print("Failed to get monster:", response.status_code)
