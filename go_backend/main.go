571232c944b4ca7a647d6e4dc9b106f83d6d9821
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-18 11:29:49 -0400

    Added random monster generator and JSON output in Go

--- go_backend/main.go
+++ go_backend/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Monster struct {
 	Name       string
@@ -11,6 +16,31 @@ type Monster struct {
 	Generation int
 }
 
+func generateRandomMonster() Monster {
+	names := []string{"Grizzle", "Zaptoad", "Blazefang", "Nibbler", "Frostjaw"}
+	colors := []string{"Red", "Blue", "Green", "Purple", "Black"}
+	rarities := []string{"Common", "Uncommon", "Rare", "Epic", "Legendary"}
+
+	rand.Seed(time.Now().UnixNano())
+
+	return Monster{
+		Name:       names[rand.Intn(len(names))],
+		Color:      colors[rand.Intn(len(colors))],
+		Strength:   rand.Intn(100) + 1, // 1–100
+		Speed:      rand.Intn(100) + 1,
+		Rarity:     rarities[rand.Intn(len(rarities))],
+		Generation: 1,
+	}
+}
+
 func main() {
-	fmt.Println("Monster struct defined.")
+	monster := generateRandomMonster()
+
+	monsterJSON, err := json.MarshalIndent(monster, "", "  ")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(string(monsterJSON))
 }
